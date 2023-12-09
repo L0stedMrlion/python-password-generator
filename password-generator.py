e999de5c40e6f8fdca913c9eed61248aa4f17c24
Mrlion <mrlion4202@gmail.com>
2023-12-09 22:01:29 +0000

    added some password gen

---
+++ password-generator.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+password = generate_password(16)
+print(password)
